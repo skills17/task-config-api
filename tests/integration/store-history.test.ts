37b92d76dff2cd0e7e403bcc5184a96d7f706f87
Cyril Wanner <info@cyr.li>
2021-12-03 20:02:00 +0000

    Update node and dependencies

--- tests/integration/store-history.test.ts
+++ tests/integration/store-history.test.ts
@@ -44,7 +44,7 @@ describe('multiple tasks', () => {
   it('stores an entry if history is enabled', async () => {
     expect(fs.existsSync(historyDirs[0])).toEqual(false);
 
-    const result = await (
+    const result = (await (
       await fetch('http://localhost:4500/test-task1/history', {
         method: 'POST',
         headers: {
@@ -52,7 +52,7 @@ describe('multiple tasks', () => {
         },
         body: JSON.stringify({ testResults: ['test'] }),
       })
-    ).json();
+    ).json()) as { created: boolean };
 
     expect(result.created).toEqual(true);
     expect(fs.existsSync(historyDirs[0])).toEqual(true);
@@ -67,7 +67,7 @@ describe('multiple tasks', () => {
   it('does not store an entry if history is disabled', async () => {
     expect(fs.existsSync(historyDirs[1])).toEqual(false);
 
-    const result = await (
+    const result = (await (
       await fetch('http://localhost:4500/test-task2/history', {
         method: 'POST',
         headers: {
@@ -75,7 +75,7 @@ describe('multiple tasks', () => {
         },
         body: JSON.stringify({ testResults: ['test'] }),
       })
-    ).json();
+    ).json()) as { created: boolean };
 
     expect(result.created).toEqual(false);
     expect(fs.existsSync(historyDirs[1])).toEqual(false);
